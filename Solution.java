a1d83ac606a3f0f82b44b1a4ffba6b22f90a9fb8
sdim-ua <ingersol@yandex.ua>
2019-01-09 00:04:54 +0400

    Create Solution.java

---
+++ Solution.java
@@ -0,0 +1,15 @@
+import java.io.*;
+
+/* 
+Финансовые ожидания
+
+*/
+
+public class Solution {
+    public static void main(String[] args) throws Exception {
+        //напишите тут ваш код
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        String n = reader.readLine();
+        System.out.println("Я буду зарабатывать " + n + " в час");
+    }
+}
